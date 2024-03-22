42230149ba9274460968bbcba50bb534bfe9e393
PabloPicas0 <pawel.fe@gmail.com>
2024-03-22 17:42:19 +0100

    feat: add solution to problem 19

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -560,19 +560,34 @@ function maximumPathSumI(triangle) {
     triangle.push(aboveLine);
   }
 
-  console.log(triangle);
   return triangle[0][0];
 }
 
+// Problem 19: Counting Sundays
+// You are given the following information, but you may prefer to do some research for yourself.
 
+// 1 Jan 1900 was a Monday.
+// Thirty days has September,
+// April, June and November.
+// All the rest have thirty-one,
+// Saving February alone,
+// Which has twenty-eight, rain or shine.
+// And on leap years, twenty-nine.
+// A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+// How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+function countingSundays(firstYear, lastYear) {
+  let sundaysCount = 0;
 
+  for (let i = firstYear; i <= lastYear; ++i) {
+    for (let j = 0; j <= 11; ++j) {
+      const day = new Date(i, j).getDay();
 
+      if (day === 0) ++sundaysCount;
+    }
+  }
 
-
-
-
-
-
+  return sundaysCount;
+}
 
 // Problem 20: Factorial digit sum
 // n! means n × (n − 1) × ... × 3 × 2 × 1
