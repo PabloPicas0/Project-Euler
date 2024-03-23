490d613d4dc11dbc73e74d4585fb5d5ee5111759
PabloPicas0 <pawel.fe@gmail.com>
2024-03-23 20:47:09 +0100

    feat: add solution to problem 21

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -604,3 +604,50 @@ function sumFactorialDigits(n: number) {
 
   return sumOfDigits;
 }
+
+// Problem 21: Amicable numbers
+// Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+
+// If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+// For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+// Evaluate the sum of all the amicable numbers under n.
+function sumAmicableNum(n) {
+  let numbers = [];
+
+  for (let i = 1; i < n; ++i) {
+    const b = d(i);
+    const a = d(b);
+    const isInAmicablePair = numbers.some((number) => {
+      const [a, b] = number;
+
+      return a === i || b === i;
+    });
+
+    if (isInAmicablePair) continue;
+
+    if (d(b) === i && a !== b) {
+      numbers.push([i, b]);
+    }
+  }
+  const sum = numbers.reduce((acc, number) => {
+    const [a, b] = number;
+
+    return (acc += a + b);
+  }, 0);
+
+  return sum;
+}
+
+function d(n) {
+  let sumOfProperDivisors = 0;
+
+  for (let i = 1; i < n; ++i) {
+    if (n % i === 0) {
+      sumOfProperDivisors += i;
+    }
+  }
+
+  return sumOfProperDivisors;
+}
