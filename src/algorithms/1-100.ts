6df84da9a3690bd271ad95f7aefda2f9ea414665
PabloPicas0 <pawel.fe@gmail.com>
2024-04-14 19:02:40 +0200

    feat: add solution to problem nr 34

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -1243,3 +1243,32 @@ function digitCancellingFractions() {
 
   return 1 / product;
 }
+
+// Problem 34: Digit factorials
+// 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
+
+// Find the numbers and the sum of the numbers which are equal to the sum of the factorial of their digits.
+
+// Note: as 1! = 1 and 2! = 2 are not sums they are not included.
+function digitFactorial() {
+  var sum = 0;
+  var numbers = [];
+
+  for (let i = 100; i <= 450585; ++i) {
+    const s = String(i)
+      .split("")
+      .map((digit) => {
+        const toFactorial = fact(Number(digit));
+
+        return toFactorial;
+      })
+      .reduce((acc, number) => acc + number);
+
+    if (s === i) {
+      numbers.push(i);
+      sum += i;
+    }
+  }
+
+  return { sum, numbers };
+}
