83472fe4cbfa7951634414c2b10482818a6cbd5b
PabloPicas0 <pawel.fe@gmail.com>
2024-04-16 21:31:50 +0200

    feat: add problem nr 36

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -1318,3 +1318,13 @@ function circularPrimes(n) {
 
   return primes.length;
 }
+
+// Problem 36: Double-base palindromes
+// The decimal number, 585 = 10010010012 (binary), is palindromic in both bases.
+
+// Find the sum of all numbers, less than n, whereas 1000 ≤ n ≤ 1000000, which are palindromic in base 10 and base 2.
+
+// (Please note that the palindromic number, in either base, may not include leading zeros.)
+function doubleBasePalindromes(n) {
+  return n;
+}
