06df090e484af7b8f55c2a38fb43577faa5ccd9e
PabloPicas0 <pawel.fe@gmail.com>
2024-04-22 17:20:57 +0200

    feat: add solution to problem nr 40

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -1475,3 +1475,28 @@ function intRightTriangles(n) {
   return highestValue;
 }
 
+// Problem 40: Champernowne's constant
+// An irrational decimal fraction is created by concatenating the positive integers:
+
+// 0.123456789101112131415161718192021...
+
+// It can be seen that the 12th digit of the fractional part is 1.
+
+// If dn represents the nth digit of the fractional part, find the value of the following expression.
+
+// d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
+function champernownesConstant(n) {
+  let fraction = ".";
+  let product = 1;
+
+  for (let i = 1; i <= n; ++i) {
+    fraction += i;
+  }
+
+  for (let i = 10; i <= n; i *= 10) {
+    console.log(fraction[i], i);
+    product *= Number(fraction[i]);
+  }
+
+  return product;
+}
