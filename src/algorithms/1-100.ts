bceaea2fdc0252795705504919a31e5333d333cd
PabloPicas0 <pawel.fe@gmail.com>
2024-04-23 23:19:01 +0200

    feat: add solution to problem nr 41

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -1499,3 +1499,38 @@ function champernownesConstant(n) {
 
   return product;
 }
+
+// Problem 41: Pandigital prime
+// We shall say that an n-digit number is pandigital if it makes use of all the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital and is also prime.
+
+// What is the largest n-length digit pandigital prime that exists?
+function pandigitalPrime(n) {
+  let pandigitalPrime = 0;
+  let minLength = "";
+  let maxLength = "";
+
+  for (let i = n, j = 1; i > 0; --i, ++j) {
+    maxLength += i;
+    minLength += j;
+  }
+
+  for (let i = Number(minLength); i < Number(maxLength); ++i) {
+    if (isPrime(i)) {
+      const s = String(i).split("");
+      let isPandigital = true;
+
+      for (let j = 1; j <= n; ++j) {
+        if (!s.includes(`${j}`)) {
+          isPandigital = false;
+          break;
+        }
+      }
+
+      if (isPandigital) {
+        pandigitalPrime = i;
+      }
+    }
+  }
+
+  return pandigitalPrime;
+}
