9247ed63371512e0cc43462e953ec2ed785cf683
PabloPicas0 <pawel.fe@gmail.com>
2024-06-03 23:29:54 +0200

    feat: add problem 53

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -2182,3 +2182,26 @@ function hasSameDigits(x, y) {
 
   return z == j;
 }
+
+// Problem 53: Combinatoric selections
+// There are exactly ten ways of selecting three from five, 12345:
+
+// 123, 124, 125, 134, 135, 145, 234, 235, 245, and 345
+// In combinatorics, we use the notation,  (53)=10
+ 
+// In general,  (nr)=n!r!(n−r)!
+//  , where  r≤n
+//  ,  n!=n×(n−1)×...×3×2×1
+//  , and  0!=1
+//  .
+
+// It is not until  n=23
+//  , that a value exceeds one-million:  (2310)=1144066
+//  .
+
+// How many, not necessarily distinct, values of  (nr)
+//   for  1≤n≤100
+//  , are greater than one-million?
+function combinatoricSelections(limit) {
+  return 1;
+}
