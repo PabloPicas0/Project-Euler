b557433e634b031a78761e2db154187262045a6b
PabloPicas0 <pawel.fe@gmail.com>
2024-06-22 21:02:29 +0200

    feat: add types to problem 40

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -1527,7 +1527,7 @@ function intRightTriangles(n) {
 // If dn represents the nth digit of the fractional part, find the value of the following expression.
 
 // d1 × d10 × d100 × d1000 × d10000 × d100000 × d1000000
-function champernownesConstant(n) {
+function champernownesConstant(n: number) {
   let fraction = ".";
   let product = 1;
 
