9f39000a599e146a66f1cb18e7f1b419cff7c663
PabloPicas0 <pawel.fe@gmail.com>
2024-07-02 14:40:59 +0200

    feat: add problem 57

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -2536,3 +2536,27 @@ function powerfulDigitSum(n) {
   console.log(maxSum);
   return maxSum;
 }
+
+
+// Problem 57: Square root convergents
+// It is possible to show that the square root of two can be expressed as an infinite continued fraction.
+
+// 2–√=1+12+12+12+…
+ 
+// By expanding this for the first four iterations, we get:
+
+// 1+12=32=1.5
+ 
+// 1+12+12=75=1.4
+ 
+// 1+12+12+12=1712=1.41666…
+ 
+// 1+12+12+12+12=4129=1.41379…
+ 
+// The next three expansions are  99/70,  239/169, and  577/408, 
+// but the eighth expansion,  1393/985, is the first example where the number of digits in the numerator exceeds the number of digits in the denominator.
+
+// In the first n expansions, how many fractions contain a numerator with more digits than denominator?
+function squareRootConvergents(n) {
+  return true;
+}
