7a302eb7dbfe6658e9612ebad3c9001e22cb2477
PabloPicas0 <pawel.fe@gmail.com>
2024-07-26 17:27:14 +0200

    feat: add problem 64

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -3144,3 +3144,127 @@ function powerfulDigitCounts(n) {
 
   return count;
 }
+
+// Problem 64: Odd period square roots
+// All square roots are periodic when written as continued fractions and can be written in the form:
+
+// √N=a0+ 1 / a1 + 1 / a2 + 1/ a3 + …
+
+// For example, let us consider √23:
+
+// √23 = 4 + √23 − 4= 4+ 1 / 1 / √23 − 4 = 4 + 1 / 1 + √23 − 3 / 7
+
+// If we continue we would get the following expansion:
+
+// √23 = 4 + 1 / 1 + 1 / 3 + 1 / 1 + 1 / 8 + …
+
+// The process can be summarized as follows:
+
+// a0=4, 1 / √23 − 4 = √23 + 4 / 7 = 1 + √23 − 3 / 7
+
+// a1=1, 7 / √23 − 3= 7(√23+3) / 14 = 3 + √23 − 3 / 2
+
+// a2=3, 2 / √23 − 3= 2(√23+3) / 14 = 1 + √23 − 4 / 7
+
+// a3=1, 7 / √23 − 4= 7(√23+4) / 7 = 8 + √23 − 4
+
+// a4=8, 1 / √23 − 4= √23+4 / 7 = 1 + √23 − 3 / 7
+
+// a5=1, 7 / √23 − 3= 7(√23+3) / 14 = 3 + √23 − 3 / 2
+
+// a6=3, 2 / √23 − 3= 2(√23+3) / 14 = 1 + √23 − 4 / 7
+
+// a7=1, 7 / √23 − 4= 7(√23+4) / 7 = 8 + √23 − 4
+
+// It can be seen that the sequence is repeating. For conciseness, we use the notation √23=[4;(1,3,1,8)]
+// to indicate that the block (1,3,1,8) repeats indefinitely.
+
+// The first ten continued fraction representations of (irrational) square roots are:
+
+// √2 = [1;(2)], period = 1
+
+// √3 = [1;(1,2)], period = 2
+
+// √5 = [2;(4)], period = 1
+
+// √6 = [2;(2,4)], period = 2
+
+// √7 = [2;(1,1,1,4)], period = 4
+
+// √8 = [2;(1,4)], period = 2
+
+// √10 = [3;(6)], period = 1
+
+// √11 = [3;(3,6)], period = 2
+
+// √12 = [3;(2,6)], period = 2
+
+// √13 = [3;(1,1,1,1,6)], period = 5
+
+// Exactly four continued fractions, for  N ≤ 13, have an odd period.
+
+// How many continued fractions for  N ≤ n have an odd period?
+
+function oddPeriodSqrts(n) {
+  // Periods of first ten continued fraction
+  // Representations square roots
+  const periods = [1, 2, 1, 2, 4, 2, 1, 2, 2, 5];
+  let N = 13;
+
+  while (N <= n) {
+    const start = Math.sqrt(N);
+    const sequence = getSequence(start, []);
+    const period = findPeriod(sequence);
+    console.log(sequence, period);
+    periods.push(period);
+
+    ++N;
+  }
+  // console.log(periods.filter(isOdd), periods.filter(isOdd).length)
+  return periods.length;
+}
+
+function getSequence(start, sequence) {
+  if (sequence.length === 9) return sequence;
+
+  const intPart = Math.floor(start);
+  const decimalPart = start % 1;
+
+  if (decimalPart === 0) {
+    sequence.push(intPart);
+    return sequence;
+  }
+
+  const recipricial = 1 / decimalPart;
+
+  sequence.push(intPart);
+
+  // console.log(start,intPart,decimalPart, recipricial)
+  // console.log(sequence)
+  return getSequence(recipricial, sequence);
+}
+
+function findPeriod(arr) {
+  let n = arr.length;
+
+  for (let period = 2; period <= n / 2; ++period) {
+    let isPattern = true;
+
+    for (let i = 1; i < n - period; i++) {
+      if (arr[i] !== arr[i + period]) {
+        isPattern = false;
+        break;
+      }
+    }
+
+    if (isPattern) {
+      return period;
+    }
+  }
+
+  return n; // If no period is found, the period is the length of the array
+}
+
+function isOdd(number) {
+  return number % 2 !== 0;
+}
