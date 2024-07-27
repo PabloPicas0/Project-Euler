669a00d0ee7dcee0423e69707741b4c92106ed3c
PabloPicas0 <pawel.fe@gmail.com>
2024-07-27 19:09:46 +0200

    feat: add types for problem 63

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -3125,7 +3125,7 @@ function sortNumbers(number: number) {
 // The 5-digit number, 16807 = 7^5, is also a fifth power. Similarly, the 9-digit number, 134217728 = 8^9, is a ninth power.
 
 // Complete the function so that it returns how many positive integers are of length n and an nth power.
-function powerfulDigitCounts(n) {
+function powerfulDigitCounts(n: number) {
   let count = 0;
   let x = 1;
 
