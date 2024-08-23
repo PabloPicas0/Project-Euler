610097eb3d9c38c373e8b0fdb4199bdea7ef76ac
PabloPicas0 <pawel.fe@gmail.com>
2024-08-23 13:05:09 +0200

    feat: solve problem 67

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -3461,7 +3461,7 @@ function decimalToFraction(value: number, donly = true) {
   var k1 = 0;
   var k2 = 1;
   var negative = false;
-  var i: number;
+  var i: number = 0;
 
   if (Number.isInteger(value)) {
     // if value is an integer, change it to fraction and stop function
@@ -3489,5 +3489,34 @@ function decimalToFraction(value: number, donly = true) {
     b = 1 / (b - a);
   } while (Math.abs(value - h1 / k1) > value * tolerance);
 
-  return (negative ? "-" : "") + (donly & (i != 0) ? i + " " : "") + (h1 == 0 ? "" : h1 + "/" + k1);
+  return (negative ? "-" : "") + (donly && i != 0 ? i + " " : "") + (h1 == 0 ? "" : h1 + "/" + k1);
+}
+
+// Problem 67: Maximum path sum II
+// By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum total from top to bottom is 23.
+
+// 3
+// 7 4
+// 2 4 6
+// 8 5 9 3
+
+// That is, 3 + 7 + 4 + 9 = 23.
+
+// Find the maximum total from top to bottom in numTriangle, a 2D array defined in the background containing a triangle with one-hundred rows.
+
+// Note: This is a much more difficult version of Problem 18. It is not possible to try every route to solve this problem, as there are 299 altogether! If you could check one trillion (1012) routes every second it would take over twenty billion years to check them all. There is an efficient algorithm to solve it. ;o)
+
+function maximumPathSumII(triangle: number[][]) {
+  while (triangle.length > 1) {
+    const lastLine = triangle.pop();
+    const aboveLine = triangle.pop();
+
+    for (let i = 0; i < aboveLine.length; ++i) {
+      aboveLine[i] = Math.max(aboveLine[i] + lastLine[i], aboveLine[i] + lastLine[i + 1]);
+    }
+
+    triangle.push(aboveLine);
+  }
+
+  return triangle[0][0];
 }
