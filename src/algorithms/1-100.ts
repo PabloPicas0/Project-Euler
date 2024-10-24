3db723703dae42c7d3f265fc50ca733850aaf5e4
PabloPicas0 <pawel.fe@gmail.com>
2024-10-24 13:45:38 +0200

    feat: add problem 79

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -4144,3 +4144,17 @@ function countPartitions(n) {
 function pentagonalNumber(k) {
   return Math.floor((k * (3 * k - 1)) / 2);
 }
+
+// Problem 79: Passcode derivation
+// A common security method used for online banking is to ask the user for three random characters from a passcode. 
+// For example, if the passcode was 531278, they may ask for the 2nd, 3rd, and 5th characters; the expected reply would be: 317.
+
+// The arrays, keylog1, keylog2, and keylog3, contains fifty successful login attempts.
+
+// Given that the three characters are always asked for in order, analyze the array so as to determine the shortest possible secret passcode of unknown length.
+const keylog1 = [
+  319,680,180,690,129,620,762,689,762,318,368,710,720,710,629,168,160,689,716,731,736,729,316,729,729,710,769,290,719,680,318,389,162,289,162,718,729,319,790,680,890,362,319,760,316,729,380,319,728,716,
+];
+passcodeDerivation(arr) {
+  return true
+}
