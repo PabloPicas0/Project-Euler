3b755d7fec80aea098d1d9b327396a9bb0fe4772
PabloPicas0 <pawel.fe@gmail.com>
2024-11-15 15:27:49 +0100

    feat: add problem 87

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -4811,3 +4811,17 @@ function countSolutions(m) {
 
   return solutions;
 }
+
+// Problem 87: Prime power triples
+// The smallest number expressible as the sum of a prime square, prime cube, and prime fourth power is 28.
+//In fact, there are exactly four numbers below fifty that can be expressed in such a way:
+
+// 28 = 22 + 23 + 24
+// 33 = 32 + 23 + 24
+// 49 = 52 + 23 + 24
+// 47 = 22 + 33 + 24
+
+// How many numbers below n can be expressed as the sum of a prime square, prime cube, and prime fourth power?
+function primePowerTriples(n) {
+  return true
+}
