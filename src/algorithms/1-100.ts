2ad04461c95ffd97568506f3ba488d486795cb61
PabloPicas0 <pawel.fe@gmail.com>
2024-12-12 14:12:30 +0100

    feat: solve problem 91

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -5147,6 +5147,34 @@ function createSet() {
 // a diagram showing the 14 triangles containing a right angle that can be formed when each coordinate is between 0 and 2
 // Given that  0≤x1,y1,x2,y2≤limit
 // How many right triangles can be formed?
+
+// For better problem understanding see:
+// www.freecodecamp.org/learn/project-euler/project-euler-problems-1-to-100/problem-91-right-triangles-with-integer-coordinates
 function rightTrianglesIntCoords(limit) {
-  return true
+  let ans = 0;
+
+  for (let y2 = 0; y2 <= limit; ++y2) {
+    for (let x2 = 1; x2 <= limit; ++x2) {
+      for (let y1 = 1; y1 <= limit; ++y1) {
+        for (let x1 = 0; x1 <= x2; ++x1) {
+          const OP = Math.sqrt((x1 - 0) ** 2 + (y1 - 0) ** 2);
+          const OQ = Math.sqrt((x2 - 0) ** 2 + (y2 - 0) ** 2);
+          const PQ = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+
+          if (PQ === 0) continue;
+
+          const [a, b, c] = [OP, OQ, PQ].sort((a, b) => a - b);
+          const isRigthTriangle = testPythagoras(a, b, c);
+
+          if (isRigthTriangle) ans += 1;
+        }
+      }
+    }
+  }
+
+  return ans;
+}
+
+function testPythagoras(a, b, c) {
+  return Math.round(c ** 2) === Math.round(a ** 2 + b ** 2);
 }
