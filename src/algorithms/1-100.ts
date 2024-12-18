47124780f1216679610ca86c1d56329f1c36e7c5
PabloPicas0 <pawel.fe@gmail.com>
2024-12-18 15:17:19 +0100

    feat: add problem 95

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -5262,8 +5262,9 @@ function arithmeticExpressions() {
   const operators = ["+", "-", "*", "/"];
   const opCombinations = getCombinations(operators);
   const digits = getAllDigits();
+  let digit = digits.next().value;
 
-  for (let digit of digits) {
+  while (digit) {
     let current = 0;
     const strDigit = digit.join("");
     const currentSet = [];
@@ -5282,6 +5283,8 @@ function arithmeticExpressions() {
       last = current;
       longestSet = strDigit;
     }
+
+    digit = digits.next().value;
   }
 
   return Number(longestSet);
@@ -5339,21 +5342,16 @@ function permuteDigits(str, currentSet, op, y = str.length, strArr = str.split("
   }
 }
 
-function getAllDigits() {
-  const sets = [];
-
+function* getAllDigits() {
   for (let d = 4; d <= 9; ++d) {
     for (let c = 3; c < d; ++c) {
       for (let b = 2; b < c; ++b) {
         for (let a = 1; a < b; ++a) {
-          const set = [a, b, c, d];
-          sets.push(set);
+          yield [a, b, c, d];
         }
       }
     }
   }
-
-  return sets;
 }
 
 function getCombinations(operators) {
@@ -5426,4 +5424,23 @@ function* getTriples(k) {
 
     n += 2;
   }
+}
+
+// Problem 95: Amicable chains
+// The proper divisors of a number are all the divisors excluding the number itself. 
+// For example, the proper divisors of 28 are 1, 2, 4, 7, and 14. As the sum of these divisors is equal to 28, we call it a perfect number.
+
+// Interestingly the sum of the proper divisors of 220 is 284 and the sum of the proper divisors of 284 is 220, forming a chain of two numbers. 
+// For this reason, 220 and 284 are called an amicable pair.
+
+// Perhaps less well known are longer chains. For example, starting with 12496, we form a chain of five numbers:
+
+// 12496→14288→15472→14536→14264(→12496→⋯)
+ 
+
+// Since this chain returns to its starting point, it is called an amicable chain.
+
+// Find the smallest member of the longest amicable chain with no element exceeding limit.
+function amicableChains(limit) {
+  return true;
 }
