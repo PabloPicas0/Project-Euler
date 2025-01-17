a6c8640baf7f91fbbafb42e002dc5d188ad406e7
PabloPicas0 <pawel.fe@gmail.com>
2025-01-17 23:08:52 +0100

    feat: add problem 99

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -5909,3 +5909,17 @@ function fasterBigIntPow(base: bigint, exp: bigint): bigint {
     return fasterBigIntPow(base * base, exp / 2n);
   }
 }
+
+// Problem 99: Largest exponential
+// Comparing two numbers written in index form like 2^11 and  3^7
+// is not difficult, as any calculator would confirm that  2^11=2048 < 3^7=2187
+
+// However, confirming that  632382518061<519432525806
+// would be much more difficult, as both numbers contain over three million digits.
+
+// Using the 2D baseExp array of base/exponent pairs, determine pair with the greatest numerical value and return it.
+
+// https://www.quora.com/How-do-I-calculate-numbers-with-high-exponents-in-the-fastest-way
+function largestExponential(baseExp) {
+  return true
+}
