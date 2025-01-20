f4029e74b7fac18123560127b86dcf469b5ca489
PabloPicas0 <pawel.fe@gmail.com>
2025-01-20 15:38:21 +0100

    feat: add problem 98

--- src/algorithms/1-100.ts
+++ src/algorithms/1-100.ts
@@ -5910,6 +5910,21 @@ function fasterBigIntPow(base: bigint, exp: bigint): bigint {
   }
 }
 
+// Problem 98: Anagramic squares
+// By replacing each of the letters in the word CARE with 1, 2, 9, and 6 respectively, we form a square number: 1296=36^2
+// What is remarkable is that, by using the same digital substitutions, the anagram, RACE, also forms a square number:  9216=96^2
+// We shall call CARE (and RACE) a square anagram word pair and specify further that leading zeroes are not permitted, 
+// neither may a different letter have the same digital value as another letter.
+
+// Using the words array, find all the square anagram word pairs (a palindromic word is NOT considered to be an anagram of itself).
+
+// What is the largest square number formed by any member of such a pair?
+
+// Note: All anagrams formed must be contained in the given words array.
+function anagramicSquares(words) {
+  return true
+}
+
 // Problem 99: Largest exponential
 // Comparing two numbers written in index form like 2^11 and  3^7
 // is not difficult, as any calculator would confirm that  2^11=2048 < 3^7=2187
