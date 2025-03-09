624fc13d4232a2ca9fdf3461818d21d74e7329d0
PabloPicas0 <pawel.fe@gmail.com>
2025-03-09 18:32:31 +0100

    feat: add problem 106

---
+++ src/algorithms/101-200/Problem_106.ts
@@ -0,0 +1,24 @@
+// Problem 106: Special subset sums: meta-testing
+// Let  S(A)
+//   represent the sum of elements in set A of size n. We shall call it a special sum set if for any two non-empty disjoint subsets, B and C, the following properties are true:
+
+// S(B)≠S(C)
+//  ; that is, sums of subsets cannot be equal.
+// If B contains more elements than C then  S(B)>S(C)
+//  .
+// For this problem we shall assume that a given set contains n strictly increasing elements and it already satisfies the second rule.
+
+// Surprisingly, out of the 25 possible subset pairs that can be obtained from a set for which n = 4, only 1 of these pairs need to be tested for equality (first rule). Similarly, when n = 7, only 70 out of the 966 subset pairs need to be tested.
+
+// For n = 12, how many of the 261625 subset pairs that can be obtained need to be tested for equality?
+
+// 613, 923, 1078, 1156, 1195, 1215, 1226, 1233, 1234, 1235, 1237, 1240
+
+// UNSOLVED
+// I can get right anwsers for n = 4,7 but don't know how to get to the right anwser for n = 12
+// Mabye Im too stupid for this right now
+function subsetSumsMetaTesting() {
+
+
+  return true;
+}
