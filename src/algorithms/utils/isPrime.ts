f1fe03bf5f349233485c7d58d31d1894e23e5b55
PabloPicas0 <pawel.fe@gmail.com>
2025-04-04 21:13:14 +0200

    feat: add isPrime method

---
+++ src/algorithms/utils/isPrime.ts
@@ -0,0 +1,10 @@
+function isPrime(num) {
+    if (num <= 1) return false; // negatives
+    if (num % 2 == 0 && num > 2) return false; // even numbers
+  
+    for (let i = 3, s = Math.sqrt(num); i <= s; i += 2) {
+      if (num % i === 0) return false;
+    }
+    
+    return true;
+  }
