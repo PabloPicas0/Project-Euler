fc89869b6c37a3183c2b1c6d1c4ac930096f51dc
PabloPicas0 <pawel.fe@gmail.com>
2025-04-18 20:14:47 +0200

    feat: add problem 114

---
+++ src/algorithms/101-200/Problem_114.ts
@@ -0,0 +1,20 @@
+// Problem 114: Counting block combinations I
+// A row measuring seven units in length has red blocks with a minimum length of three units placed on it,
+// such that any two red blocks (which are allowed to be different lengths) are separated by at least one black square.
+// There are exactly seventeen ways of doing this.
+// See: https://projecteuler.net/problem=114
+
+// Possible ways of placing block with a minimum length of three units, on a row with length of seven units
+// How many ways can a row measuring fifty units in length be filled?
+
+// Note: Although the example above does not lend itself to the possibility, in general it is permitted to mix block sizes.
+// For example, on a row measuring eight units in length you could use red (3), black (1), and red (4).
+
+// Solution is based on count recurrence relation
+// It can be found here
+// https://codereview.stackexchange.com/questions/227624/counting-block-combinations-i-project-euler-114#mjx-eqn-f4
+// Also it is not optimized for space efficiency
+
+function countingBlockOne() {
+  return true
+}
