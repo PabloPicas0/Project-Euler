96ce475af8829475c9b30edc26596dcf1ade9299
PabloPicas0 <pawel.fe@gmail.com>
2025-04-23 22:09:01 +0200

    feat: add problem 116

---
+++ src/algorithms/101-200/Problem_116.ts
@@ -0,0 +1,23 @@
+// Problem 116: Red, green or blue tiles
+// A row of five black square tiles is to have a number of its tiles replaced with coloured oblong tiles chosen from
+// red (length two), green (length three), or blue (length four).
+
+// If red tiles are chosen there are exactly seven ways this can be done.
+
+// Possible ways to placing red oblong on a row with length of five units
+// If green tiles are chosen there are three ways.
+
+// Possible ways of placing green oblong on a row with length of five units
+// And if blue tiles are chosen there are two ways.
+
+// Possible ways of placing blue oblong on a row with length of five units
+// Assuming that colors cannot be mixed there are 7 + 3 + 2 = 12 ways of replacing the black tiles in a row measuring five units in length.
+// How many different ways can the black tiles in a row measuring fifty units in length be replaced if colors cannot be mixed and at least one colored tile must be used?
+
+// Note: This is related to Problem 117.
+
+function redGreenBlueOne() {
+  return true
+}
+
+
