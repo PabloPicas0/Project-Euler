ccb53c2f84fcf48cf8e987284288f8664fb332ce
PabloPicas0 <pawel.fe@gmail.com>
2025-04-26 16:52:56 +0200

    refactor: solve problem 116

--- src/algorithms/101-200/Problem_116.ts
+++ src/algorithms/101-200/Problem_116.ts
@@ -17,7 +17,38 @@
 // Note: This is related to Problem 117.
 
 function redGreenBlueOne() {
-  return true
+  return F(50, 2) + F(50, 3) + F(50, 4);
 }
 
+function count(n, m) {
+  if (n < m) return 0;
+  if (n === m) return 1;
 
+  return count(n - 1, m) + count(n - m, m) + 1;
+}
+
+function F(n, m) {
+  const size = m + 1;
+  const arr = new Array(size).fill(0);
+
+  let p = 0;
+  let l = arr.length - 1;
+  let k = arr.length - m;
+
+  arr[p] = 1;
+
+  for (let i = m; i < n + 1; ++i) {
+    const next = arr[l] + arr[k] + 1;
+    arr[p] = next;
+
+    p = (p + 1) % size;
+    l = (l + 1) % size;
+    k = (k + 1) % size;
+  }
+
+  arr.sort((a, b) => a - b);
+
+  const last = arr.length - 1;
+
+  return arr[last];
+}
