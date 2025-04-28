69970f2a360872af53e9ce9304bcf886de35b0e1
PabloPicas0 <pawel.fe@gmail.com>
2025-04-28 22:40:31 +0200

    feat: add problem 117

---
+++ src/algorithms/101-200/Problem_117.ts
@@ -0,0 +1,19 @@
+// Problem 117: Red, green, and blue tiles
+// Using a combination of black square tiles and oblong tiles chosen from:
+// red tiles measuring two units,
+// green tiles measuring three units, and blue tiles measuring four units,
+// it is possible to tile a row measuring five units in length in exactly fifteen different ways.
+
+// Possible ways of placing red, green and blue oblongs on a row with length of five units
+// How many ways can a row measuring fifty units in length be tiled?
+
+// Note: This is related to Problem 116.
+
+// Numbers in this problem are Tetranacci numbers
+// They can be found here:
+// https://oeis.org/A000078
+// https://oeis.org/A000078/b000078.txt
+
+ function redGreenBlueTilesTwo() {
+  return true
+}
