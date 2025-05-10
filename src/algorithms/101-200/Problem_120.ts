eaa4d828a8e22bf73bf6da558de10e4619749da7
PabloPicas0 <pawel.fe@gmail.com>
2025-05-10 20:03:17 +0200

    feat: add problem 120

---
+++ src/algorithms/101-200/Problem_120.ts
@@ -0,0 +1,13 @@
+// Problem 120: Square remainders
+// Let r be the remainder when (a−1)^n+(a+1)^n is divided by  a^2
+
+// For example, if a=7 and  n=3 then r=42:63+83=728≡42 mod 49
+//  And as n varies, so too will r, but for  a=7
+//  it turns out that  r_max=42
+
+// For  3 ≤ a ≤ 1000
+// find  ∑ r_max
+function squareRemainders() {
+
+  return true;
+}
