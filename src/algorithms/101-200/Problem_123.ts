a61756695934fcf25427f45cabff396b3290801e
PabloPicas0 <pawel.fe@gmail.com>
2025-05-30 21:18:51 +0200

    feat: add problem 123

---
+++ src/algorithms/101-200/Problem_123.ts
@@ -0,0 +1,10 @@
+// Problem 123: Prime square remainders
+
+// Let p_n be the nth prime: 2, 3, 5, 7, 11, ..., and let r be the remainder when (p_n−1)^n+(p_n+1)^n is divided by p_n^2.
+// For example, when n=3, p3=5, and 43+63=280≡5 mod 25.
+// The least value of n for which the remainder first exceeds 109 is 7037.
+// Find the least value of n for which the remainder first exceeds 1010.
+
+function primeSquareRemainders() {
+  return true;
+}
