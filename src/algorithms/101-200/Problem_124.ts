2de42ab6026eb3059c6e6666d59eb9ed27c673bb
PabloPicas0 <pawel.fe@gmail.com>
2025-06-02 22:35:42 +0200

    refactor: add types for problem 124

--- src/algorithms/101-200/Problem_124.ts
+++ src/algorithms/101-200/Problem_124.ts
@@ -1,6 +1,6 @@
 // Problem 124: Ordered radicals
 
-// The radical of n, rad(n), is the product of the distinct prime factors of n. 
+// The radical of n, rad(n), is the product of the distinct prime factors of n.
 // For example, 504=23×32×7, so rad(504)=2×3×7=42.
 
 // If we calculate rad(n) for 1≤n≤10, then sort them on rad(n), and sorting on n if the radical values are equal, we get:
@@ -18,10 +18,32 @@
 //   9	          3		    7	          7	    9
 //   10	          10		  10	        10	  10
 
-// Let E(k) be the kth element in the sorted n column; 
-// for example, E(4)=8 and E(6)=9. 
+// Let E(k) be the kth element in the sorted n column;
+// for example, E(4)=8 and E(6)=9.
 // If rad(n) is sorted for 1≤n≤100000, find E(10000).
 
+import { primeFactors } from "../utils/primeFactors.ts";
+import { sieve } from "../utils/sieve.ts";
+
 function orderedRadicals() {
-  return true
+  const limit = 100000;
+  const searched = 10000 - 1;
+  const primes = sieve(200000);
+  const rads = [[1, 1]];
+
+  for (let i = 2; i <= limit; ++i) {
+    rads.push([i, rad(i, primes)]);
+  }
+
+  rads.sort((a, b) => a[1] - b[1]);
+
+  return rads[searched][0];
+}
+
+function rad(n: number, primes: number[]) {
+  const factors = primeFactors(n, primes);
+  const uniqueFactors = new Set(factors);
+  const product = [...uniqueFactors].reduce((acc, fact) => acc * fact);
+
+  return product;
 }
